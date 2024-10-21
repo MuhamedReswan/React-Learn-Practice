073b91a6e628d1e6133717ed4c2972f83337d117
MuhamedReswan <muhamedreswan9917@gmail.com>
2024-10-21 14:25:08 +0530

    css style completed

---
+++ component with Css/src/Components/Button/Button.jsx
@@ -0,0 +1,45 @@
+// // MODULE CSS STYLE
+
+// import style from "./Button.module.css";
+
+// function Button() {
+//     return (
+//         <button className={style.button}>Click Me</button>
+//     );
+// }
+
+// export default Button;
+
+
+
+
+
+// // INLINE CSS STYLE
+
+// function Button() {
+
+//     const style ={
+
+//             backgroundcolor: "hsl(200, 100%, 50%)",
+//             color: "white",
+//             padding: "10px 20px",
+//             borderradius: "5px",
+//             cursor: "pointer",
+//             border: "none",
+//     }
+//     return (
+//         <button className={style}>Click Me</button>
+//     );
+// }
+
+// export default Button;
+
+
+
+
+
+function Button() {
+  return <button className="button">Click Me</button>;
+}
+
+export default Button;
