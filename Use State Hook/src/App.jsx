a6ff50f1fd19b4676367fc0afd3a69b4d4563046
MuhamedReswan <muhamedreswan9917@gmail.com>
2024-10-22 10:35:39 +0530

    useState completed

---
+++ Use State Hook/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import MyComponent from './Components/MyComponent'
+import Counter from './Components/Counter'
+
+function App() {
+
+  return (
+    <>
+   <MyComponent/>
+   <hr />
+   <Counter/>
+   </>
+  )
+}
+
+export default App
